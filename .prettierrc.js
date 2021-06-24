5eade9d76c3add8524f533cf8a2d7aafee23f796
Carlos A. Chávez Rosario <66329126+cchavezmx@users.noreply.github.com>
2021-06-24 02:33:12 -0500

    exporando el componente

--- .prettierrc.js
+++ .prettierrc.js
@@ -9,4 +9,7 @@ module.exports = {
       },
     },
   ],
+  "prettier/prettier": ["erro", {
+    endOfLine: "auto"
+  }]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,9 +20,9 @@ export default function Home({ posts, pagination }) {
       <WebsiteJsonLd siteTitle={title} />
       <Header>
         <h1
-          className={styles.title}
+          className={styles.testtitle}
           dangerouslySetInnerHTML={{
-            __html: title,
+            __html: 'Grupo Intecsa',
           }}
         />
 
